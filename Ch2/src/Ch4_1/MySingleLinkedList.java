d513673bc9c31a519d815023ff08b63eae1fb7a5
MinkyungJ <jmk976@gmail.com>
2022-03-17 14:02:52 +0900

    4-1 LinkedList(4,5/5)

--- Ch2/src/Ch4_1/MySingleLinkedList.java
+++ Ch2/src/Ch4_1/MySingleLinkedList.java
@@ -11,7 +11,7 @@ public class MySingleLinkedList<T> {
         size = 0;
     }
 
-    public void addFrist( T item ){
+    public void addFirst( T item ){
         Node<T> newNode = new Node<T>(item);     // T: type parameter
         newNode.next = head;
         head = newNode;
@@ -22,13 +22,24 @@ public class MySingleLinkedList<T> {
     }
 
     public void addAfter( Node<T> before, T item ){
-        Node<T> tmp = new Node<T>();
+        Node<T> tmp = new Node<T>( item );
         tmp.next = before.next;
         before.next = tmp;
 
         size++;
     }
     //addBefore은 구현하기 간단하지 않다.. add한것 before의 자리를 알아내기 위해 첫번째 자리부터 검색해가야한다
+
+    public void add( int index, T item ){   // insert
+        if( index < 0 || index > size)
+            return;
+        if ( index == 0)
+            addFirst( item );
+        else{
+            Node<T> q = getNode(index-1);
+            addAfter( q, item );
+        }
+    }
 
     public T removeFirst(){
         if(head == null){
@@ -51,20 +62,65 @@ public class MySingleLinkedList<T> {
         return temp;
     }
     
-    public void add( int index, T item ){   // insert
 
+    public T remove( int index ){   // delete
+        if( index < 0 || index >= size )
+            return null;
+        if( index == 0 )
+            return removeFirst();
+        Node<T> prev = getNode( index-1 );
+        return removeAfter( prev );
     }
 
-    public void remove( int index ){   // delete
+    public T remove( T item ){   // delete
+        Node<T> p = head; 
+        Node<T> q = null;
+        while( p!= null && !p.data.equals( item )){
+            q = p;
+            p = p.next;
+        }
+        if( p == null )
+            return null;
+        if( q == null )
+            return removeFirst();
+        return removeAfter(q);  
+      }
 
+
+    public int indexOf( T item ){  // search
+        Node<T> p = head; //첫번째 노드 가리키기
+        int index = 0;
+        while ( p!=null ){
+            if(p.data.equals(item)){
+                return index;
+            }
+            p = p.next;
+            index++;
+        }
+        return -1;
+    }
+    
+    public Node<T> getNode( int index ){
+        if (index < 0 || index >= size){
+            return null;
+        }
+        Node<T> p = head;
+        for(int i=0; i<index; i++){
+            p = p.next;
+        }
+        return p; 
     }
 
     public T get( int index ){
-        return null;
-    }
-
-    public int indexOf( T item ){  // search
-        return -1;
+        if (index < 0 || index >= size){
+            return null;
+        }
+        // Node<T> p = head;
+        // for(int i=0; i<index; i++){
+        //     p = p.next;
+        // }
+        // return p.data; 
+      return getNode(index).data;
     }
 
     public static void main(String[] args){
