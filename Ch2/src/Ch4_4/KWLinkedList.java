ce3e17d25169716cc3ac5c9f1bf6df3c318ea345
MinkyungJ <jmk976@gmail.com>
2022-03-22 10:01:21 +0900

    4-4(3/4)

--- Ch2/src/Ch4_4/KWLinkedList.java
+++ Ch2/src/Ch4_4/KWLinkedList.java
@@ -49,44 +49,73 @@ public class KWLinkedList <E>{
         private Node<E> lastItemReturned;
         private int index;
 
-        public KWListIterator( int index ){
+        public KWListIterator( int i ){
+            if( i < 0 || i > size ){
+                throw new IndexOutOfBoundsException("Invalid index "+ i);
+            }
+            lastItemReturned = null;
+            if( i == size ){
+                index = size;
+                nextItem = null;
+            } else {
+                nextItem = head;
+                for( index = 0; index < i; index++ ){
+                    nextItem = nextIem.next;
+                }
+            }
 
         }
 
         @Override
         public boolean hasNext() {
             // TODO Auto-generated method stub
-            return false;
+            return nextItem != null;
         }
 
         @Override
         public E next() {
             // TODO Auto-generated method stub
-            return null;
+            if(!hasNext())
+                throw new NoSuchElmentException();
+
+            lastItemReturned = nextItem;
+            nextItem = nextItem.next;
+            index++;
+
+            return lastItemReturned.data;
         }
 
         @Override
         public boolean hasPrevious() {
             // TODO Auto-generated method stub
-            return false;
+            return (nextItem == null && size != 0) || nextItem.previous != null;
         }
 
         @Override
         public E previous() {
             // TODO Auto-generated method stub
-            return null;
+            if(!hasPrevious())
+                throw new NoSuchElementException();
+            if(nextItem == null) //Iterator past the last element
+                nextItem = tail;
+            else
+                nextItem =  nextItem.previous;
+            lastItemReturned = nextItem;
+            index--;
+            
+            return lastItemReturned.data;
         }
 
         @Override
         public int nextIndex() {
             // TODO Auto-generated method stub
-            return 0;
+            return index;
         }
 
         @Override
         public int previousIndex() {
             // TODO Auto-generated method stub
-            return 0;
+            return index-1;
         }
 
         @Override
@@ -103,7 +132,28 @@ public class KWLinkedList <E>{
 
         @Override
         public void add(E e) {
-            // TODO Auto-generated method stub
+            if( head == null ){
+                head = new Node<E>(e);
+                tail = head;
+            } else if( nextItem == head ){
+                Node<E> newNode = new Node<E>(e);
+                newNode.next = nextItem;
+                nextItem.previous = newNode;
+                head = newNode;
+            } else if( nextItem == null ){
+                Node<E> newNode = new Node<E>(e);
+                tail.next = newNode;
+                newNode.previous = tail;
+                tail = newNode;
+
+            } else{
+                Node<E> newNode = new Node<E>(e);
+                newNode.previous = nextItem.previous;
+                nextItem.previous.next = newNode;
+                newNode.next = nextItem;
+                nextItem.previous = newNode;
+            }
+            size++; index++;
             
         }
 
