8bab63419dc2e19de7a934b6b79419f735453c2d
MinkyungJ <jmk976@gmail.com>
2022-04-18 09:58:45 +0900

    hash_table

---
+++  Master-the-Coding-Interview-Data-Structures-and-Algorithms-using-JAVA/src/Sec6_Arrays/MyNode.java
@@ -0,0 +1,30 @@
+package Sec6_Arrays;
+
+public class MyNode{
+    public String key;
+    public int value;
+  
+    public MyNode(String key, int value){
+      this.key = key;
+      this.value = value;
+    }
+  
+    public String getKey(){
+      return key;
+    }
+    public void setKey(String key){
+      this.key = key;
+      
+    }
+  
+     public int getValue(){
+      return value;
+    }
+  
+    public void setValue(int value){
+      this.value = value;
+    }
+  
+    
+    
+  }
