d504a7483005e84e444a9289b3a7810ccbbe7d80
MinkyungJ <jmk976@gmail.com>
2022-05-17 17:31:13 +0900

    Tree

---
+++  Master-the-Coding-Interview-Data-Structures-and-Algorithms-using-JAVA/src/Sec10_Tree/HelperFunctions.java
@@ -0,0 +1,12 @@
+package Sec10_Tree;
+
+public class HelperFunctions {
+
+     // traverse in order
+
+     
+    // traverse pre order
+
+     // traverse post order
+    
+}
---
+++  Master-the-Coding-Interview-Data-Structures-and-Algorithms-using-JAVA/src/Sec10_Tree/Node.java
@@ -0,0 +1,15 @@
+package Sec10_Tree;
+
+public class Node {
+    int value;
+    Node right;
+    Node left;
+    
+    public Node(int value){
+        this.value = value;
+        this.right = right;
+        this.left = left;
+    }
+}
+    
+
