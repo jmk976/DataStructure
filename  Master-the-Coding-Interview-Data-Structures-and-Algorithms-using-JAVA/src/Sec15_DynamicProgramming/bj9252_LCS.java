d431471880d3b4a6be0d253e0b438982ada17705
MinkyungJ <jmk976@gmail.com>
2022-06-08 17:44:50 +0900

    DP

---
+++  Master-the-Coding-Interview-Data-Structures-and-Algorithms-using-JAVA/src/Sec15_DynamicProgramming/bj9252_LCS.java
@@ -0,0 +1,56 @@
+package Sec15_DynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class bj9252_LCS {
+    //2차원 점화식 배열
+    private static long[][] DP;
+    //LCS 저장하기 리스트
+    private static ArrayList<Character> Path;
+    //A :1번째 문자열   B :2번째 문자열
+    private static char[] A;
+    private static char[] B;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        A = br.readLine().toCharArray();
+        B = br.readLine().toCharArray();
+        DP = new long[A.length + 1][B.length +1];
+        Path = new ArrayList<Character>();
+        for(int i = 1; i <= A.length; i++){
+            for(int j = 1; j <= B.length; j++ ){
+                //A[i]와  B[i]가 같으면 DP[i][j]의 값을 왼쪽 대각선 값 +1로 저장하기 
+                if(A[i - 1] == B[j -1]) {
+                    DP[i][j] = DP[i-1][j-1] + 1;
+                //다른 경우에는 왼쪽이 값과 위의 값 중 큰 값으로 DP[i][j] 채우기 
+                } else {
+                    DP[i][j] = Math.max(DP[i-1][j], DP[i][j-1]);
+                }
+            }
+        }
+
+        System.out.println(DP[A.length][B.length]);
+        getText(A.length, B.length); 
+        for(int i = Path.size() -1; i >= 0; i --){
+            System.out.print(Path.get(i));
+        }
+        System.out.println();
+
+    }
+    private static void getText(int r, int c){   //LCS 출력하기 함수
+        if(r == 0 || c == 0) return;
+        if(A[r-1] == B[c-1]){  // 같으면  LCS에 기록하고 왼쪽
+            Path.add(A[r-1]);
+            getText(r-1, c-1);
+        } else {
+            if(DP[r-1][c] > DP[r][c-1]) // 다르면 왼쪽과 중 큰 수로 이동
+                getText(r-1, c);
+            else    
+                getText(r, c-1);
+        }
+    }
+
+    
+}
